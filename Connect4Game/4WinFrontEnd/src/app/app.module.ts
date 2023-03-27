356cead19791e51df46aa6382039eb1cbd243905
Lisa Chandra <lisa.chandra@infoteam.de>
2023-03-27 11:56:11 +0200

    added queue components

--- Connect4Game/4WinFrontEnd/src/app/app.module.ts
+++ Connect4Game/4WinFrontEnd/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { GameComponent } from './game/game.component';
+import { QueueComponent } from './queue/queue.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    GameComponent
+    GameComponent,
+    QueueComponent
   ],
   imports: [
     BrowserModule,
