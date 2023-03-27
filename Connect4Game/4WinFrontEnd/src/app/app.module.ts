37c7e1c05bea8776b3c7bb6d71011bc9c4f1667c
Lisa Chandra <lisa.chandra@infoteam.de>
2023-03-27 16:26:55 +0200

    added routing in app-routing.module and refactored app.module for routing

--- Connect4Game/4WinFrontEnd/src/app/app.module.ts
+++ Connect4Game/4WinFrontEnd/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { GameComponent } from './game/game.component';
-import { QueueComponent } from './queue/queue.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    GameComponent,
-    QueueComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
