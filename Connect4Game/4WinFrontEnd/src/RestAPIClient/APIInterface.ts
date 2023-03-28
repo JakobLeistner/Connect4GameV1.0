c36be3251fc10acfc43b8b43c3182f1d9476ed78
Lukas Spindler <lukas.spindler@infoteam.de>
2023-03-28 09:41:37 +0200

    include APIInterface

--- Connect4Game/4WinFrontEnd/src/RestAPIClient/APIInterface.ts
+++ Connect4Game/4WinFrontEnd/src/RestAPIClient/APIInterface.ts
@@ -18,4 +18,48 @@ export class APIInterface{
         .set("rtpconnentionid", rtpConnectionID)
         ));
     }
+    public CreateGame(player: MyPlayer): Observable<any>{
+        return this.apiHttpRequest.Post("",this.apiUriBuilder.GetAPIUri("CreateGame",
+        new Map()
+        .set("player", player)
+        ));
+    }
+    public JoinGame(p1: MyPlayer, waitingGameListIndex: number): Observable<JoinGameResponse>{
+        return this.apiHttpRequest.Post("", this.apiUriBuilder.GetAPIUri("JoinGame",
+        new Map()
+        .set("p1", p1)
+        .set("waitingGameListIndex", waitingGameListIndex)
+        ));
+    }
+    public DoMove(column: number, gameID: string, player: MyPlayer): Observable<any>{
+        return this.apiHttpRequest.Post("", this.apiUriBuilder.GetAPIUri("DoMove",
+        new Map()
+        .set("column", column)
+        .set("gameID", gameID)
+        .set("player", player)
+        ));
+    }
+    public LeaveGame(player: MyPlayer, gameID: string): Observable<any>{
+        return this.apiHttpRequest.Post("", this.apiUriBuilder.GetAPIUri("LeaveGame",
+        new Map()
+        .set("player", player)
+        .set("gameID", gameID)
+        ))
+    }
+    public GetWaitingGames(): Observable<WaitingGamesResponse>{
+        return this.apiHttpRequest.Get(this.apiUriBuilder.GetAPIUri("GetWaitingGames", new Map()))
+    }
+    public GetGameInfo(gameID: string, playerID: string): Observable<GameInfoResponse>{
+        return this.apiHttpRequest.Get(this.apiUriBuilder.GetAPIUri("GetGameInfo",
+        new Map()
+        .get(gameID)
+        .get(playerID)
+        ))
+    }
+    public PlayerHasAlreadyWaitingGame(playerID: string): Observable<any>{
+        return this.apiHttpRequest.Get(this.apiUriBuilder.GetAPIUri("PlayerHasAlreadyWaitingGame",
+        new Map()
+        .get(playerID)
+        ))
+    }
 }
